833aecb6f29b3a7b0174b1bed24cf3c142a18657
Sparsh Mishra <sparsh.mishra@igniteconnex.com>
2024-07-29 12:10:07 +0530

    added health monitoring

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
 	"elvy-loadbalancer/config"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"elvy-loadbalancer/loadbalancer"
 )
@@ -22,17 +25,41 @@ func main() {
 
 	lb := loadbalancer.NewLoadBalancerServers(config.Port, servers)
 
+	server := &http.Server{
+		Addr: ":" + config.Port,
+		// Handler: nil,
+	}
+
 	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
 		log.Printf("Received request: %s %s\n", req.Method, req.URL)
 		lb.ServeProxy(rw, req)
 	}
 
 	http.HandleFunc("/", handleRedirect)
+
+	// this function will start the server in a goroutine
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil && err != http.ErrServerClosed {
+			log.Fatalf("Error starting server: %v", err)
+		}
+	}()
+
 	fmt.Printf("forwarding request to address %q\n", lb.Port())
 
-	err = http.ListenAndServe(":"+config.Port, nil)
-	if err != nil {
-		fmt.Println("Error starting server: ", err)
+	// creating channelm to listen for os signals
+
+	c := make(chan os.Signal, 1)
+
+	signal.Notify(c, os.Interrupt)
+
+	// Block until a signal is received
+	<-c
+	fmt.Println("Shutting down server...")
+	error := server.Shutdown(context.Background())
+	if error != nil {
+		log.Fatalf("Error shutting down server: %v", error)
 	}
+	fmt.Println("Server gracefully stopped")
 
 }
